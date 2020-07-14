c280c2497d3469996111da87b88efd9af86a9adf
Kamye <andrej15ganzha@gmail.com>
2020-07-14 12:49:05 +0300

    router Guard

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,6 +8,7 @@ import { DashboardPageComponent } from './dashboard-page/dashboard-page.componen
 import { EditPageComponent } from './edit-page/edit-page.component';
 import { OrdersPageComponent } from './orders-page/orders-page.component';
 import { FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { AuthGuard } from "src/app/shared/auth.guard";
 
 @NgModule({
   declarations: [
@@ -26,10 +27,10 @@ import { FormsModule, ReactiveFormsModule} from "@angular/forms";
       {path: '', component: AdminLayoutComponent, children: [
         {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
         {path: 'login', component: LoginPageComponent},
-        {path: 'add', component: AddPageComponent},
-        {path: 'dashboard', component: DashboardPageComponent},
-        {path: 'product/:id/edit', component: EditPageComponent},
-        {path: 'orders', component: OrdersPageComponent}
+        {path: 'add', component: AddPageComponent, canActivate: [AuthGuard]},
+        {path: 'dashboard', component: DashboardPageComponent, canActivate: [AuthGuard]},
+        {path: 'product/:id/edit', component: EditPageComponent, canActivate: [AuthGuard]},
+        {path: 'orders', component: OrdersPageComponent, canActivate: [AuthGuard]}
       ]}
     ])
   ],
